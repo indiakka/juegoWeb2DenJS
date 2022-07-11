1aa7ab6cf5b5d5d6995d47fda9f93e001b95fcc1
indiakka <78439130+indiakka@users.noreply.github.com>
2022-07-11 16:42:02 +0200

    Escalar, rotar y definir el punto de referencia de un Sprite

---
+++ Phaser-SetUp/src/GamePlay.js
@@ -0,0 +1,52 @@
+GamePlayManager = {
+  init: function () {
+    //redimensiona la imagen al cambiar el tamaño de la pantalla
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    //alinear la imagen horizonal y verticalmente
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+  },
+  preload: function () {
+    game.load.image("background", "assets/images/background.png");
+    /*la imagen del caballo contiene dos imagenes diferentes, para poder alternar 
+    entre una y otra, después de la ruta se pone el ancho y el alto de la imagen 
+    y el 2 sería el nº de imagenes que contiene la imagen */
+    this.horse = game.load.spritesheet(
+      "horse",
+      "assets/images/horse.png",
+      84,
+      156,
+      2
+    );
+   },
+  create: function () {
+    //añadimos las imagenes
+    game.add.sprite(0, 0, "background");
+    this.horse = game.add.sprite( 0, 0, 'horse' )
+   /*al ponerle el frame en 1 añade la parte de la imagen secundaria
+     si se le añade el 0 muestra la parte de la primera imagen */
+    this.horse.frame = 0;
+    //para modificar la posición de la imagen
+    this.horse.x = game.width / 2;
+    this.horse.y = game.height / 2;
+    /* para que el caballo esté justo en el centro, ya que el sprite
+    lo coloca en su centro, pero no de la propia imagen, al cambiar el 
+    anchor se cambia al centro de la propia imagen del caballo */
+    this.horse.anchor.setTo( 0.5 )
+    //modificamos el ángulo de la imagen
+    this.horse.angle = 0
+    //modificamos el tamaño del caballo
+    this.horse.scale.setTo( 2 )
+    //modifica la visibilidad de la imagen, 0 es invisible y 1 visible
+    this.horse.alpha = 0
+  },
+  update: function ()
+  {
+    this.horse.angle += 1
+  },
+};
+
+var game = new Phaser.Game(1136, 640, Phaser.AUTO);
+
+game.state.add("gameplay", GamePlayManager);
+game.state.start("gameplay");
