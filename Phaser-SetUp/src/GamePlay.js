dc476b48e7d553ed1b0984f2e342df93fb21f502
indiakka <78439130+indiakka@users.noreply.github.com>
2022-07-11 16:58:53 +0200

    Movimiento del Caballo

--- Phaser-SetUp/src/GamePlay.js
+++ Phaser-SetUp/src/GamePlay.js
@@ -5,6 +5,8 @@ GamePlayManager = {
     //alinear la imagen horizonal y verticalmente
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
+    //que el caballo no se mueva hasta hacer click en pantalla
+    this.flagFirstMouseDown = false;
   },
   preload: function () {
     game.load.image("background", "assets/images/background.png");
@@ -18,12 +20,12 @@ GamePlayManager = {
       156,
       2
     );
-   },
+  },
   create: function () {
     //añadimos las imagenes
     game.add.sprite(0, 0, "background");
-    this.horse = game.add.sprite( 0, 0, 'horse' )
-   /*al ponerle el frame en 1 añade la parte de la imagen secundaria
+    this.horse = game.add.sprite(0, 0, "horse");
+    /*al ponerle el frame en 1 añade la parte de la imagen secundaria
      si se le añade el 0 muestra la parte de la primera imagen */
     this.horse.frame = 0;
     //para modificar la posición de la imagen
@@ -32,17 +34,41 @@ GamePlayManager = {
     /* para que el caballo esté justo en el centro, ya que el sprite
     lo coloca en su centro, pero no de la propia imagen, al cambiar el 
     anchor se cambia al centro de la propia imagen del caballo */
-    this.horse.anchor.setTo( 0.5 )
+    this.horse.anchor.setTo(0.5);
     //modificamos el ángulo de la imagen
-    this.horse.angle = 0
+    //this.horse.angle = 0
     //modificamos el tamaño del caballo
-    this.horse.scale.setTo( 2 )
+    //this.horse.scale.setTo( 2 )
     //modifica la visibilidad de la imagen, 0 es invisible y 1 visible
-    this.horse.alpha = 0
+    //this.horse.alpha = 0
+  //para capturar un click en pantalla
+    game.input.onDown.add(this.onTap, this);
   },
-  update: function ()
-  {
-    this.horse.angle += 1
+  //que al hacer click en pantalla el caballo se mueva
+  onTap: function () {
+    this.flagFirstMouseDown = true;
+  },
+
+  update: function () {
+    if (this.flagFirstMouseDown === true) {
+      //para que siga las coordenadas del mouse
+      var pointerX = game.input.x;
+      var pointerY = game.input.y;
+      //calcular la distancia entre nuestro mouse y el caballo
+      var distX = pointerX - this.horse.x;
+      var distY = pointerY - this.horse.y;
+      /*saber si el caballo está a la derecha del ratón o a la izqda
+     para que el caballo mire hacia la drcha o la izqda */
+      if (distX > 0) {
+        this.horse.scale.setTo(1, 1);
+      } else {
+        this.horse.scale.setTo(-1, 1);
+      }
+      /*para que el caballo se mueva a nuestra posición de mouse
+    el numero del final será la distancia con que se mueve  */
+      this.horse.x += distX * 0.02;
+      this.horse.y += distY * 0.02;
+    }
   },
 };
 
